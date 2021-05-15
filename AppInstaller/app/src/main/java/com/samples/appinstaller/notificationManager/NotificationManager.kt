320d2d9a2f925d16949c96f645afcaa66d08577b
Yacine Rezgui <rezgui.y@gmail.com>
2021-05-16 00:38:24 +0100

    Add NotificationManager

---
+++ AppInstaller/app/src/main/java/com/samples/appinstaller/notificationManager/NotificationManager.kt
@@ -0,0 +1,45 @@
+package com.samples.appinstaller.notificationManager
+
+import android.app.NotificationChannel
+import android.app.NotificationManager.IMPORTANCE_HIGH
+import android.app.PendingIntent
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.samples.appinstaller.R
+
+class NotificationManager(private val context: Context) {
+    private val notificationManager: NotificationManagerCompat by lazy {
+        NotificationManagerCompat.from(context)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createChannel(id: String, name: String, description: String) {
+        val channel = NotificationChannel(id, name, IMPORTANCE_HIGH).apply {
+            this.description = description
+        }
+
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    fun createAndShowNotification(
+        id: Int,
+        title: String,
+        description: String,
+        channel: String,
+        contentIntent: PendingIntent?
+    ) {
+        val builder = NotificationCompat.Builder(context, channel)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(description)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(contentIntent)
+            .setAutoCancel(true)
+
+        notificationManager.notify(id, builder.build())
+
+    }
+}
